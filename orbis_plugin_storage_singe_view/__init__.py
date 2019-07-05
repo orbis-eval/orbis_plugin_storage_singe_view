e491fc1fff88415907081a755bb73f806e36ca70
fabod <2189668+fabod@users.noreply.github.com>
2019-07-05 14:41:27 +0200

    wip

--- orbis_plugin_storage_singe_view/__init__.py
+++ orbis_plugin_storage_singe_view/__init__.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+
+__name__ = "orbis_plugin_storage_single_view"
+__version__ = "1.1.dev"
+__author__ = "fabod"
+__year__ = "2019"
+__description__ = "The single_view storage plugin for Orbis"
+__license__ = "GPL2"
+__min_python_version__ = "3.6"
+__requirements_file__ = "requirements.txt"
+__url__ = "https://github.com/orbis-eval/orbis_plugin_storage_single_view"
+__type__ = "plugin"
+
 from .main import SingleView as Main
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 import os
 
 try:
-    from orbis.core.plugin_setup import PluginSetupBaseClass
-    PluginSetupBaseClass().run(os.path.dirname(os.path.realpath(__file__)))
+    from orbis.core.orbis_setup import OrbisSetupBaseClass
+    OrbisSetupBaseClass().run(os.path.dirname(os.path.realpath(__file__)))
 except Exception as exception:
     print("Orbis not found. Please install Orbis first.")
     print(f"({exception})")
+
